2ce26d8df8b8e1d06d4fa9656ccdc315ccbacaf1
Unknown <james@varah.com>
2017-10-17 16:55:16 -0700

    Added Intake Subsystem

---
+++ src/org/usfirst/frc/team5026/robot/subsystems/Intake.java
@@ -0,0 +1,23 @@
+package src.org.usfirst.frc.team5026.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	Spark leftMotor;
+	Spark rightMotor;
+	public Intake(Spark leftMotor, Spark rightMotor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
